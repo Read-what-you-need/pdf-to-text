bcf6c7057c3eb08c8e86586940d562b4900e2074
Deepak Garasangi <31365513+soulbliss@users.noreply.github.com>
2021-02-08 19:34:51 +0530

    Update create_env.sh

--- create_env.sh
+++ create_env.sh
@@ -1,3 +1,9 @@
+
 #!/bin/bash
-touch .env
-echo 'REDIS_HOST=$(REDIS_HOST)' >> .env
+> .env
+echo 'REDIS_HOST='$REDIS_HOST >> .env
+echo 'REDIS_PORT='$REDIS_PORT >> .env
+echo 'REDIS_PASSKEY='$REDIS_PASSKEY >> .env
+echo 'AWS_ACCESS_KEY_ID='$AWS_ACCESS_KEY_ID >> .env
+echo 'AWS_SECRET_ACCESS_KEY='$AWS_SECRET_ACCESS_KEY >> .env
+echo 'AWS_REGION_NAME='$AWS_REGION_NAME >> .env
